f49da63bd188fa7edef2f47a1191b93064a9b7be
Ruban <65346010+rubansk@users.noreply.github.com>
2020-06-08 14:20:51 +0800

    Adding HotProspects

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by SK Ruban on 8/6/20.
+//  Copyright © 2020 SK Ruban. All rights reserved.
+//
+
+import SwiftUI
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
+struct ContentView: View {
+    @State private var selectedTab = "zero"
+
+    var body: some View {
+        Text("Hello, World!")
+        .onAppear {
+            self.fetchData(from: "https://www.apple.com") { result in
+                switch result {
+                case .success(let str):
+                    print(str)
+                case .failure(let error):
+                    switch error {
+                    case .badURL:
+                        print("Bad URL")
+                    case .requestFailed:
+                        print("Network problems")
+                    case .unknown:
+                        print("Unknown error")
+                    }
+                }
+            }
+        }
+        }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        // check the URL is OK, otherwise return with a failure
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            // the task has completed – push our work back to the main thread
+            DispatchQueue.main.async {
+                if let data = data {
+                    // success: convert the data to a string and send it back
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    // any sort of network failure
+                    completion(.failure(.requestFailed))
+                } else {
+                    // this ought not to be possible, yet here we are
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
