cd9a93488ed145baf97a305b07ceca23b7a42ce2
Ruban <65346010+rubansk@users.noreply.github.com>
2020-06-09 16:31:35 +0800

    Adding HotProspects

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,58 +7,19 @@
 //
 
 import SwiftUI
-
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
+import SamplePackage
 
 struct ContentView: View {
-    @State private var selectedTab = "zero"
-
-    var body: some View {
-        Text("Hello, World!")
-        .onAppear {
-            self.fetchData(from: "https://www.apple.com") { result in
-                switch result {
-                case .success(let str):
-                    print(str)
-                case .failure(let error):
-                    switch error {
-                    case .badURL:
-                        print("Bad URL")
-                    case .requestFailed:
-                        print("Network problems")
-                    case .unknown:
-                        print("Unknown error")
-                    }
-                }
-            }
-        }
-        }
+    let possibleNumbers = Array(1...60)
     
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        // check the URL is OK, otherwise return with a failure
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            // the task has completed – push our work back to the main thread
-            DispatchQueue.main.async {
-                if let data = data {
-                    // success: convert the data to a string and send it back
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // any sort of network failure
-                    completion(.failure(.requestFailed))
-                } else {
-                    // this ought not to be possible, yet here we are
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings
+    }
+    
+    var body: some View {
+        Text(results)
     }
 }
 
